fef622e08189ac97332eaad266d4e42d24a100bd
Josh Y <kauhentus@gmail.com>
2023-06-10 09:50:13 -0400

    tweaked memory + added memory tracker

--- src/util/draw.ts
+++ src/util/draw.ts
@@ -1,24 +1,36 @@
 import { Color } from "./color"
 import { createCanvas } from "@napi-rs/canvas";
-import * as fs from "fs";
+
+const squareSize = 250;
+const indices = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+const canvases = indices.map(v => {
+    const canvas = createCanvas(
+        squareSize * v,
+        squareSize
+    );
+    const ctx = canvas.getContext('2d');
+    return {
+        canvas: canvas,
+        ctx: ctx
+    }
+});
 
 export class Drawer {
     static async squares(colors: Color[]){
-        const squareSize = 250;
-        const canvas = createCanvas(
-            squareSize * colors.length,
-            squareSize
-        );
-        const ctx = canvas.getContext('2d');
+        const memTracker = process.memoryUsage();
+        console.log("memory", JSON.stringify(memTracker));
+
+        const {
+            canvas: canvas,
+            ctx: ctx
+        } = canvases[colors.length - 2];
 
         for(let i = 0; i < colors.length; i++){
             ctx.fillStyle = '#' + colors[i].getHexString(false);
             ctx.fillRect(i * squareSize, 0, squareSize, squareSize);
         }
 
-        /*canvas.encode('png').then(data => {
-            fs.writeFile('./picture.png', data, () => {});
-        })*/
         const imageData = await canvas.encode('png');
         return imageData;
     }
